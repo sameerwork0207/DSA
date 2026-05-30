0b56ae89de76fe11e6881719b5206494937c64fe
Sameer Shaikh <sam7778886667@gmail.com>
2026-05-30 18:08:31 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (86.86%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -6,8 +6,10 @@ class Solution {
         int i = m + n - 1;
 
         while (p2 >= 0) {
+            //if p2 ends then program stops running, remaining elements in nums1 are already sorted and at correct place
 
             if (p1 >= 0 && nums1[p1] > nums2[p2]) {
+                //if p1 ends then we stop checking for elements in nums1, keep executing the else condition below until the p2 also ends
                 nums1[i] = nums1[p1];
                 p1--;
             } else {
@@ -16,6 +18,7 @@ class Solution {
             }
 
             i--;
+            
         }
     }
 }
