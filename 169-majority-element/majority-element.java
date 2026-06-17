2ed41054f6a01094b466c345eace89999433f735
Sameer Shaikh <sam7778886667@gmail.com>
2026-06-17 10:35:27 +0530

    Time: 1 ms (99.88%) | Memory: 55.7 MB (31.41%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,34 +1,20 @@
 class Solution {
     public int majorityElement(int[] nums) {
 
-        if (nums.length==0){
-            return 0;
-        }
-
-           int maj=nums[0];
-           int votes=0;
-            
-
-        
+        if (nums.length==0) return 0;
+        int maj=nums[0];
+        int votes=0;
+             
         for(int i =0;i<nums.length;i++){
 
-            if (nums[i]==maj){
-                votes++;
-            }
-            else{
-                votes--;}
+            if (nums[i]==maj) votes++;
+            else votes--;
             
             if (votes<1){
                 maj=nums[i];
                 votes=1;
             }
-
         }
-
-
-
-
-        return maj;
-        
+        return maj;    
     }
 }
